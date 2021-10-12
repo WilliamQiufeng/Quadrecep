ba4b47874a2097462636e44d7e73826ac9bcee92
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2021-10-12 21:11:05 +0800

    Add Time and Factor fields to ScrollVelocity.cs

--- Scripts/Map/ScrollVelocity.cs
+++ Scripts/Map/ScrollVelocity.cs
@@ -1,7 +1,11 @@
-namespace Quadrecep.Scripts.Map
+using System;
+using YamlDotNet.Serialization;
+
+namespace Quadrecep.Map
 {
     public class ScrollVelocity
     {
-        
+        public float Time;
+        public float Factor;
     }
 }
