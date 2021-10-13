345be58959cd7e1219b99038dc16d5c1ce2dc949
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2021-10-13 19:36:12 +0800

    Change test map content

--- Scripts/CreateMap.cs
+++ Scripts/CreateMap.cs
@@ -16,12 +16,15 @@ public class CreateMap : Node
         _map.CreateMap("Test");
         _map.MapSet.AudioPath = "テレキャスターヒーホーイlong ver  すりぃ feat鏡音レン.mp3";
         _map.MapSet.BackgroundPath = "Telecaster_B-Boy_highres.jpg";
-        var path = new DirectionObject(new int[]{1, 1, 0, 1});
-        _map.GetMap(0).AddNote(new NoteObject(1000, 0, path.RawDirection));
+        _map.GetMap(0).AddNote(new NoteObject(1000, 0, 0b1011));
+        _map.GetMap(0).AddNote(new NoteObject(2000, 0, 0b1110));
+        _map.GetMap(0).AddNote(new NoteObject(3000, 0, 0b1010));
+        _map.GetMap(0).AddNote(new NoteObject(4000, 0, 0b1000));
+        _map.GetMap(0).AddSV(new ScrollVelocity(500, 0.5f));
+        _map.GetMap(0).AddSV(new ScrollVelocity(1500, 1.5f));
+        _map.GetMap(0).AddSV(new ScrollVelocity(3000, 1f));
+        _map.GetMap(0).AddSV(new ScrollVelocity(3600, 2.0f));
         _map.SaveMap();
-        var verifyPath = new DirectionObject(path.RawDirection);
-        GD.Print("Direction: " + string.Join(", ", verifyPath.Direction));
-        GD.Print("VecDirection: " + verifyPath.NetDirection);
     }
 
     // Called when the node enters the scene tree for the first time.
