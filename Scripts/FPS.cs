0d249cc6734b66babae60feb0b81ba716da4acf1
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2021-10-19 16:53:26 +0800

    Add FPS Display

---
+++ Scripts/FPS.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public class FPS : Label
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        var fps = ((int) Engine.GetFramesPerSecond()).ToString().PadLeft(4);
+        Text = $"{fps} FPS";
+    }
+}
