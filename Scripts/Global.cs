8f3ad3a417c2f9486475608a2e8590c86c1a8d0b
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2021-10-22 07:58:13 +0800

    Move Nodes to Quadrecep namespace

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -1,21 +1,24 @@
 using Godot;
 using Quadrecep.Database;
 
-public class Global : Node
+namespace Quadrecep
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public class Global : Node
     {
-        DatabaseHandler.Initialize();
-        OS.VsyncEnabled = false;
-    }
+        // Declare member variables here. Examples:
+        // private int a = 2;
+        // private string b = "text";
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            DatabaseHandler.Initialize();
+            OS.VsyncEnabled = false;
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(float delta)
+        {
+        }
     }
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,23 +1,25 @@
 using Godot;
-using Quadrecep;
 
-public class Player : Control
+namespace Quadrecep
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public class Player : Control
     {
-    }
+        // Declare member variables here. Examples:
+        // private int a = 2;
+        // private string b = "text";
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        var play = GetNode<Play>("../..");
-        if (play.Finished) return;
-        RectPosition = play.CurrentPath.GetPosition(play.Time);
-        // GetNode<Camera2D>("../../Camera2D").Offset = RectPosition;
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(float delta)
+        {
+            var play = GetNode<Play>("../..");
+            if (play.Finished) return;
+            RectPosition = play.CurrentPath.GetPosition(play.Time);
+            // GetNode<Camera2D>("../../Camera2D").Offset = RectPosition;
+        }
     }
 }
