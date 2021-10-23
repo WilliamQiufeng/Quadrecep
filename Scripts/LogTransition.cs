11f97a9107a8a21cc5d83c86c17a3a7e21fbeca0
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2021-10-23 12:26:18 +0800

    Add LogTransition

---
+++ Scripts/LogTransition.cs
@@ -0,0 +1,23 @@
+using System;
+using Godot;
+
+namespace Quadrecep
+{
+    public class LogTransition
+    {
+        private readonly float _startTime;
+        private readonly float _length;
+
+        public LogTransition(float startTime, float length)
+        {
+            _startTime = startTime;
+            _length = length;
+        }
+
+        public float GetCurrent(float time)
+        {
+            var val = Math.Log(time - _startTime + 1, _length + 1);
+            return double.IsNaN(val)? 0 : Mathf.Clamp((float) val, 0, 1);
+        }
+    }
+}
