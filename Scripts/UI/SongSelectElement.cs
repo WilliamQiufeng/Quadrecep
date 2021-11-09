18a39ad723fed07c3a20c41b0cce751e06d791bb
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2021-11-09 19:07:05 +0800

    Support selection with ui_left and ui_right, difficulty selection with ui_up and ui_down

--- Scripts/UI/SongSelectElement.cs
+++ Scripts/UI/SongSelectElement.cs
@@ -10,10 +10,23 @@ namespace Quadrecep.UI
         private static readonly Color FocusedColor = new(1, 1, 1, 0);
 
         public static PackedScene Scene;
+        public int _difficultyIndex;
         private MapSetObject _map;
-        public int DifficultyIndex;
+        public int Index;
         public string MapFile;
         public PackedScene PlayScene;
+
+        public int DifficultyIndex
+        {
+            get => _difficultyIndex;
+            set
+            {
+                _difficultyIndex = value;
+                GetNode<Label>("Difficulty").Text = _map.Maps[DifficultyIndex].DifficultyName;
+            }
+        }
+
+        public int Count => _map.Maps.Count;
 
         public override void _Ready()
         {
@@ -23,7 +36,7 @@ namespace Quadrecep.UI
             GetNode<Label>("Name").Text = _map.Name;
             GetNode<Label>("Author").Text = _map.Creator;
             GetNode<Label>("Artist").Text = _map.Artist;
-            GetNode<Label>("Difficulty").Text = _map.Maps[DifficultyIndex].DifficultyName;
+            DifficultyIndex = 0;
             GetNode<AudioStreamPlayer>("Player").Stream =
                 Play.LoadAudio(Global.RelativeToMap(MapFile, _map.AudioPath));
             // GrabFocus();
@@ -31,7 +44,11 @@ namespace Quadrecep.UI
 
         public override void _Process(float delta)
         {
-            if (HasFocus() && Input.IsActionPressed("ui_play")) PlayMap();
+            if (!HasFocus()) return;
+            if (Input.IsActionJustPressed("ui_up"))
+                DifficultyIndex = DifficultyIndex - 1 < 0 ? Count - 1 : DifficultyIndex - 1;
+            if (Input.IsActionJustPressed("ui_down")) DifficultyIndex = (DifficultyIndex + 1) % Count;
+            if (Input.IsActionPressed("ui_play")) PlayMap();
         }
 
         public void _OnPlayPressed()
@@ -45,16 +62,19 @@ namespace Quadrecep.UI
             GetNode<AudioStreamPlayer>("Player").Stop();
             var play = PlayScene.Instance<Play>();
             play.MapFile = MapFile;
+            play.MapIndex = DifficultyIndex;
             GetTree().Root.AddChild(play);
             GetParent().GetParent().GetParent().GetParent().QueueFree();
         }
 
         public void _OnFocusEnter()
         {
+            GetParent().GetParent().GetParent<SongSelectSlider>().MapIndex = Index;
             var tween = GetNode<Tween>("Mask/Tween");
             tween.InterpolateProperty(tween.GetParent(), "modulate", UnfocusedColor, FocusedColor, FocusDuration,
                 Tween.TransitionType.Linear, Tween.EaseType.In);
             tween.Start();
+            FadeIn();
         }
 
         public void _OnFocusExit()
@@ -63,6 +83,28 @@ namespace Quadrecep.UI
             tween.InterpolateProperty(tween.GetParent(), "modulate", FocusedColor, UnfocusedColor, FocusDuration,
                 Tween.TransitionType.Linear, Tween.EaseType.In);
             tween.Start();
+            FadeOut();
+        }
+
+        private void FadeOut()
+        {
+            var tween = GetNode<Tween>("Player/Tween");
+            tween.InterpolateProperty(tween.GetParent(), "volume_db", 0, -80, FocusDuration);
+            tween.Start();
+        }
+
+        private void FadeIn()
+        {
+            GetNode<AudioStreamPlayer>("Player").Playing = true;
+            GetNode<AudioStreamPlayer>("Player").Seek(_map.PreviewTime / 1000);
+            var tween = GetNode<Tween>("Player/Tween");
+            tween.InterpolateProperty(tween.GetParent(), "volume_db", -6, 0, FocusDuration);
+            tween.Start();
+        }
+
+        public void _OnTweenComplete(Object obj, NodePath key)
+        {
+            if (!HasFocus()) GetNode<AudioStreamPlayer>("Player").Stop();
         }
     }
 }
--- Scripts/UI/SongSelectSlider.cs
+++ Scripts/UI/SongSelectSlider.cs
@@ -5,22 +5,39 @@ namespace Quadrecep.UI
     public class SongSelectSlider : Control
     {
         public readonly Directory ContainingDirectory = new();
+        private int _mapIndex;
+
+        public int MapIndex
+        {
+            get => _mapIndex;
+            set
+            {
+                _mapIndex = value;
+                UpdateElementFocus();
+            }
+        }
+
+        private HBoxContainer HBoxContainer => GetNode<HBoxContainer>("ScrollContainer/HBoxContainer");
+
+        public int Count => HBoxContainer.GetChildren().Count;
 
         public override void _Ready()
         {
             ContainingDirectory.Open($"user://{Map.Map.MapDirectory}");
             LoadElements(ContainingDirectory);
+            MapIndex = 0;
         }
 
         public void RefreshElements()
         {
             ClearChildren();
             LoadElements(ContainingDirectory);
+            MapIndex = 0;
         }
 
         private void ClearChildren()
         {
-            foreach (Node child in GetNode<HBoxContainer>("ScrollContainer/HBoxContainer").GetChildren())
+            foreach (Node child in HBoxContainer.GetChildren())
                 child.QueueFree();
         }
 
@@ -36,11 +53,24 @@ namespace Quadrecep.UI
                     GD.Print(fileName);
                     element.MapFile = fileName;
                     element.PlayScene = Play.Scene;
-                    GetNode<HBoxContainer>("ScrollContainer/HBoxContainer").AddChild(element);
+                    element.Index = Count;
+                    HBoxContainer.AddChild(element);
                 }
 
                 fileName = dir.GetNext();
             }
+        }
+
+        public override void _Process(float delta)
+        {
+            // if (!HasFocus()) return;
+            if (Input.IsActionJustPressed("ui_left")) MapIndex = MapIndex - 1 < 0 ? Count - 1 : MapIndex - 1;
+            if (Input.IsActionJustPressed("ui_right")) MapIndex = (MapIndex + 1) % Count;
+        }
+
+        private void UpdateElementFocus()
+        {
+            ((Control) HBoxContainer.GetChildren()[MapIndex]).GrabFocus();
         }
     }
 }
