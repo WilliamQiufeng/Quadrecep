0f1225679dbf561caa59d97c5acab065092c5ba1
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2021-12-01 16:26:27 +0800

    Use _Input to retrieve inputs. Rename InputMap "play_{direction}" to "play_{index}"

--- Scripts/Gameplay/InputRetriever.cs
+++ Scripts/Gameplay/InputRetriever.cs
@@ -6,20 +6,17 @@ namespace Quadrecep.Gameplay
     {
         public APlay APlayParent { get; set; }
 
-        public override void _Process(float delta)
+        public override void _Input(Godot.InputEvent @event)
         {
-            var processor = APlayParent.InputProcessor;
-            var time = APlayParent.Time;
-            EnqueueInputs(ref processor, ref time, "play_left", 0);
-            EnqueueInputs(ref processor, ref time, "play_down", 1);
-            EnqueueInputs(ref processor, ref time, "play_up", 2);
-            EnqueueInputs(ref processor, ref time, "play_right", 3);
+            for (var i = 0; i < 4; i++)
+                EnqueueInputs(APlayParent.InputProcessor, APlayParent.Time, @event, $"play_{i}", i);
         }
 
-        private static void EnqueueInputs(ref AInputProcessor processor, ref float time, string actionName, int key)
+        private static void EnqueueInputs(AInputProcessor processor, float time, Godot.InputEvent @event,
+            string actionName, int key)
         {
-            if (Input.IsActionJustPressed(actionName)) processor.Inputs[key].Enqueue(new InputEvent(time, key, false));
-            if (Input.IsActionJustReleased(actionName)) processor.Inputs[key].Enqueue(new InputEvent(time, key, true));
+            if (@event.IsActionPressed(actionName)) processor.Inputs[key].Enqueue(new InputEvent(time, key, false));
+            if (@event.IsActionReleased(actionName)) processor.Inputs[key].Enqueue(new InputEvent(time, key, true));
         }
     }
 }
