b5abf19509a0e64a0be43357772994427330c700
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2021-12-09 11:52:21 +0800

    Update FPS once per second

--- Scripts/Fps.cs
+++ Scripts/Fps.cs
@@ -4,18 +4,11 @@ namespace Quadrecep
 {
     public class Fps : Label
     {
-        // Declare member variables here. Examples:
-        // private int a = 2;
-        // private string b = "text";
-
-        // Called when the node enters the scene tree for the first time.
-        public override void _Ready()
-        {
-        }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+        private int _count = -1;
+        
         public override void _PhysicsProcess(float delta)
         {
+            if ((_count = (_count + 1) % 60) != 0) return;
             var fps = ((int) Engine.GetFramesPerSecond()).ToString().PadLeft(4);
             Text = $"{fps} FPS";
         }
